ce36900929e6808f8ca12890675612ad388d1b1e
redtomatoL <redtomatoL@163.com>
2019-08-21 22:09:50 +0800

    解决spring-security 5.0 密码必须加密问题

--- src/main/java/com/ikaiyong/lawssecurity/User/MyUserDetailService.java
+++ src/main/java/com/ikaiyong/lawssecurity/User/MyUserDetailService.java
@@ -1,11 +1,13 @@
 package com.ikaiyong.lawssecurity.User;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
@@ -15,11 +17,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MyUserDetailService implements UserDetailsService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        String password = passwordEncoder.encode("123456");
         log.info("登陆用户名"+s);
+        log.info("用户密码为："+password);
 
-        return new User(s,"123456",AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return new User(s,password,AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
--- src/main/java/com/ikaiyong/lawssecurity/configer/BrowserConfigure.java
+++ src/main/java/com/ikaiyong/lawssecurity/configer/BrowserConfigure.java
@@ -1,12 +1,22 @@
 package com.ikaiyong.lawssecurity.configer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 public class BrowserConfigure extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 
     @Override
